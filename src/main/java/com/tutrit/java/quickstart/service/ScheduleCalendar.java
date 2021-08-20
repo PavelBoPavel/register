c409f379a580a3c9e7f1cb085ae4a516ed1592fd
PS <pava.bore@gmail.com>
2021-08-20 10:17:29 +0300

    FBB-10_add slot feature

---
+++ src/main/java/com/tutrit/java/quickstart/service/ScheduleCalendar.java
@@ -0,0 +1,22 @@
+package com.tutrit.java.quickstart.service;
+
+import com.tutrit.java.quickstart.bean.Slot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScheduleCalendar {
+    private static final List<Slot> scheduleCalendarList = new ArrayList();
+
+    public void addSlot(Slot slot) {
+        scheduleCalendarList.add(slot);
+    }
+
+    public List<Slot> findAll(){
+        return scheduleCalendarList;
+    }
+
+    public void remuveAllSlots() {
+        scheduleCalendarList.removeAll(findAll());
+    }
+}
